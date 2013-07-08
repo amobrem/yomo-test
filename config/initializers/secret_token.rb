23929f33277e7ee97d2aed2d0ef6a145a2ace3c3
Shawn C Partridge <shwn.partridge@gmail.com>
2013-07-08 01:26:18 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yomo2::Application.config.secret_token = '20667c317835576f1f5a03d33dca4509525db3290427a8d34b3f107f08b8296499536aa24eb85080b3eedef11fb3fa68a8dade20d950a13110b19e31ab67b376'
